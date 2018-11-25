6f425c7eb1a263aefa246b73d4a43c56bc892386
Sergii <sergiy.stotskiy@gmail.com>
2018-11-25 11:30:46 +0200

    feat(ability): configures ability version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ability } from '@casl/ability'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-test';
+  isVisibleTitle = true
+
+  constructor(private ability: Ability) {}
+
+  toggle() {
+    if (this.ability.rules.length) {
+      this.ability.update([])
+    } else {
+      this.ability.update([
+        { actions: 'manage', subject: 'customer' }
+      ])
+    }
+  }
 }
